2ab66535827287d8d5b28c12af83b131622fe48e
iEdwinT <edwin_torres@icloud.com>
2020-01-23 19:30:46 -0500

    Finished assessment.

--- app.js
+++ app.js
@@ -54,9 +54,9 @@ Wagon.prototype = {
 };
 
 // * Test Code * //
-// Create a wagon that can hold 2 people
+// ! Create a wagon that can hold 2 people
 let wagon = new Wagon(2);
-// Create three travelers
+// ! Create three travelers
 let henrietta = new Traveler("Henrietta");
 let juan = new Traveler("Juan");
 let maude = new Traveler("Maude");
@@ -64,10 +64,10 @@ console.log(`${wagon.getAvailableSeatCount()} should be 2`);
 wagon.join(henrietta);
 console.log(`${wagon.getAvailableSeatCount()} should be 1`);
 wagon.join(juan);
-wagon.join(maude); // There isn't room for her!
+wagon.join(maude); // ! There isn't room for her!
 console.log(`${wagon.getAvailableSeatCount()} should be 0`);
 henrietta.hunt(); // get more food
 juan.eat();
-juan.eat(); // juan is now hungry (sick)
+juan.eat(); // ! juan is now hungry (sick)
 console.log(`${wagon.shouldQuarantine()} should be true since juan is sick`);
 console.log(`${wagon.totalFood()} should be 3`);
